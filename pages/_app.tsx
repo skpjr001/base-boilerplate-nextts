5901cadeb680767c46a80cf0c05f6c128fab59d8
Sachin Kumar <sachinkumarskrose@gmail.com>
2021-11-30 11:44:59 +0530

    added settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
+//Nextjs
 import { AppProps } from 'next/app'
 
+import superjson from "superjson"
+
+//Seo
 import { DefaultSeo } from "next-seo";
 import { seoConfig } from "@lib/config/next-seo.config";
 
+//Intl(i18)
 import { appWithTranslation } from 'next-i18next'
 
+//styles
 import "../styles/fonts.css";
 import "../styles/globals.css";
 
